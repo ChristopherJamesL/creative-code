96a31e99d3ebf8efed0b91aad0a4fa0b395ae303
ChristopherL <anbchris@gmail.com>
2026-06-26 23:09:16 -0400

    edit get endpoint on frontend to include /api

--- client/src/features/auth/api/auth.api.ts
+++ client/src/features/auth/api/auth.api.ts
@@ -1,7 +1,7 @@
 import apiClient from "../../../api/axios";
 
 export const httpLogin = async () => {
-  const response = await apiClient.get("/auth/login");
+  const response = await apiClient.get("/api/auth/login");
   console.log("response: ", response);
 
   return response.data.data;
