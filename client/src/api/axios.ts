b50556f429c67372b7805a05cdd3aa3b0942f1fd
ChristopherL <anbchris@gmail.com>
2026-06-28 18:56:07 -0400

    edit server url for safari compatability

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -5,4 +5,17 @@ const apiClient = axios.create({
   withCredentials: true,
 });
 
+apiClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (
+      error.response?.status === 401 &&
+      !window.location.pathname.startsWith("/login")
+    ) {
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  }
+);
+
 export default apiClient;
